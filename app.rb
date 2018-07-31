fe02a889c327c80b60a6eef3b45204094df52f19
Kayvan Vakili <kvakili813@gmail.com>
2018-07-31 22:30:57 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,11 +3,6 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    require './environment'
-
-    module FormsLab
-      class App < Sinatra::Base
-
         get '/' do
           erb :root
         end
